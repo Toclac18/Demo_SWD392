9f2ab9a0249041e32c65435603e59d291a307c15
duongnk203 <duongnkhe171819@fpt.edu.vn>
2025-03-25 01:33:11 +0700

    Code duongBranch

---
+++ Demo_SWD392_Coding/Controllers/MedicalRecordController.cs
@@ -0,0 +1,32 @@
+﻿using Demo_SWD392_Coding.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_SWD392_Coding.Controllers
+{
+    public class MedicalRecordController : Controller
+    {
+        private readonly IMedicalRecordRepository _medicalRecordRepository;
+        private readonly IMedicalRecordDetailRepository _medicalRecordDetailRepository;
+        public MedicalRecordController(IMedicalRecordRepository medicalRecordRepository, IMedicalRecordDetailRepository medicalRecordDetailRepository)
+        {
+            _medicalRecordRepository = medicalRecordRepository;
+            _medicalRecordDetailRepository = medicalRecordDetailRepository;
+        }
+        // GET: MedicalRecord
+        public ActionResult Index()
+        {
+            var medicalRecords = _medicalRecordRepository.GetMedicalRecords();
+            return View(medicalRecords);
+        }
+
+        // GET: MedicalRecord/Details/5
+        public ActionResult Details(string id)
+        {
+            var medicalRecordDetails = _medicalRecordDetailRepository.GetMedicalRecords(id.ToString());
+            return View(medicalRecordDetails);
+        }
+
+        
+    }
+}
---
+++ Demo_SWD392_Coding/ViewModels/MedicalRecordDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo_SWD392_Coding.ViewModels
+{
+    public class MedicalRecordDetailModel
+    {
+        public string RecordDetailCode { get; set; } = null!;
+        public string? AppointmentCode { get; set; }
+        public string RecordCode { get; set; } = null!;
+        public string? DoctorCode { get; set; }
+        public string? Result { get; set; }
+        public string? DoctorName { get; set; }
+        public string? PatientName { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
